eadab19b7722edaa76fb1c192ee87d98eb9f20f4
The-Impredecible-Magician <angelpaucarhuamanlazo@gmail.com>
2023-12-09 00:53:57 -0500

    probando rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('cursos', function() {
+    return "Otra cosita mk";
+});
+
+Route::get('cursos/create', function(){
+    return "Here you could create whatever you want";
+});
+
+/*Route::get('cursos/{curso}', function($curso){
+    return "welcome to: $curso course";
+});*/
+
+Route::get('cursos/{curso}/{categoria?}', function($curso, $categoria = null) {
+    if($categoria){
+        return "welcome to: $curso course in the $categoria category";
+    }
+    else{return "welcome to: $curso course";
+    }
+    
+});
