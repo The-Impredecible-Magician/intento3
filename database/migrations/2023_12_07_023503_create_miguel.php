b9d4257dec3b931c3220ab3e5c1b1fd3c8b08906
The-Impredecible-Magician <angelpaucarhuamanlazo@gmail.com>
2023-12-08 19:58:39 -0500

    lo logreeee

--- database/migrations/2023_12_07_023503_create_miguel.php
+++ database/migrations/2023_12_07_023503_create_miguel.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('miguel', function (Blueprint $table) {
             $table->id();
             $table->string('cuys');
+            $table->string('perros');
             $table->timestamps();
 
             $table->foreignId('id_cuys')
